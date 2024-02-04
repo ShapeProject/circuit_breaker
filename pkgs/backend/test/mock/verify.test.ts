9e6f39042d3acdb5cebc517022ec17e2942bf5d1
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-02-04 13:22:07 +0900

    update testCode

--- pkgs/backend/test/mock/verify.test.ts
+++ pkgs/backend/test/mock/verify.test.ts
@@ -1,14 +1,19 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
+import fs from "fs";
 import { ethers } from "hardhat";
+import * as snarkjs from "snarkjs";
 import {
   MockVerifier,
   MockVerifier__factory,
 } from "../../typechain-types";
 
-
 describe("MockVerifier", function () {
- 
+  // file paths
+  const INPUT_FILE_PATH = "./zk/mock/input.json";
+  const WASM_FILE_PATH = "./zk/mock/circuit.wasm";
+  const ZKEY_FILE_PATH = "./zk/mock/zkey.zkey";
+
   /**
    *  Verifier deploy function
    * @returns
@@ -31,27 +36,29 @@ describe("MockVerifier", function () {
     it("verify proof", async function () {
       // deploy contract
       const { verifier } = await loadFixture(deployContract);
+
+      // input data
+      const inputFile: any = fs.readFileSync(INPUT_FILE_PATH)
+      const inputData = JSON.parse(inputFile);
+      // generate proof & publicSignals
+      const { 
+        proof, 
+        publicSignals 
+      } = await snarkjs.groth16.fullProve(
+        inputData, 
+        WASM_FILE_PATH, 
+        ZKEY_FILE_PATH
+      );
+      // generate callData for Verifier Contract
+      const callData = await snarkjs.groth16.exportSolidityCallData(proof, publicSignals);
+      // convert
+      const solidityCallData = JSON.parse(`[${callData}]`);
       // verify & Mint NFT
       const result = await verifier.verifyProof(
-        [
-          "0x21dd170b9f3f6e07ac843245ad126df0e5d483a11189634af9c3f6332c20c718", 
-          "0x0c694227b621c1b231548795dd119dc4ad63e6a361c1075d81e7630da13fc96d"
-        ],[
-          [
-            "0x3007930b71aa37567e88d9dc84d2b324cc09bbb8d203fefe37a04ca83e78b2bb", 
-            "0x09305ded2c52c61d91dae25f3d35d9f1e749e3214760dd72e6196fd08c01a0da"
-          ],[
-            "0x28dbf94ade7a100d948c54b856e13a26862a1824725ee9fc1d3f2f7106000643", 
-            "0x095677b2c0688931ec1fd37f9f9ca8d76cbaeedd28209203e79323ee4c8db30f"
-          ]
-        ],
-        [
-          "0x2f9dce209936fffdf4a8e3d5fdccaafaf148054525b48c90f0e8279008369ab1", 
-          "0x19c36c806927a7706612319b54137e7072601281a140897ee58f3902017d78f7"
-        ],[
-          "0x000000000000000000000000000000000000000000000000000000000000003a",
-          "0x0000000000000000000000000000000000000000000000000000000000000003"
-        ]
+        solidityCallData[0],
+        solidityCallData[1],
+        solidityCallData[2],
+        solidityCallData[3]
       );
       // check initial data
       expect(true).to.eql(result);

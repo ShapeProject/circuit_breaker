d8f53850f2848702572d796c85e31769eb5285e0
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-02-06 22:49:37 +0900

    delete sandbox codes

--- pkgs/privy-pimlico-demo/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { Home } from "@/components/home"
-
-export default function Main() {
-    return <Home />
-}
--- pkgs/smartwallet-demo/components/navbar.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css";
-
-export default function Navbar() {
-    return (
-        <div className={styles.navbar}>
-            <h3>TW Monsters</h3>
-        </div>
-    )
-};

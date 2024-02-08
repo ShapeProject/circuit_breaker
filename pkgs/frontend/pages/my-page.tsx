462b838822061512d0d380b6d60f91546c228824
Mameta29 <fc.kikkawa@gmail.com>
2024-02-08 09:08:47 +0900

    made pages

---
+++ pkgs/frontend/pages/my-page.tsx
@@ -0,0 +1,9 @@
+
+export default function MyPage() {
+  return (
+    <>
+        <h1>My Page</h1>
+        <p>This is my page</p>
+    </>
+  )
+}
---
+++ pkgs/frontend/pages/verify.tsx
@@ -0,0 +1,10 @@
+
+export default function Verify() {
+    return (
+      <>
+          <h1>Verify</h1>
+          <p>This is Verify ページ</p>
+      </>
+    )
+  }
+  

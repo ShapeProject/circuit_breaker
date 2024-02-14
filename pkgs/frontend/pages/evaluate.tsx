f0345151c03f84aab2afe45d877add9b7ec80f07
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-02-14 21:27:49 +0900

    add call metatransaction function

--- pkgs/frontend/pages/evaluate.tsx
+++ pkgs/frontend/pages/evaluate.tsx
@@ -1,7 +1,80 @@
-import { NavigationSidebar } from "@/components/navigation/navigationSidebar";
 import Input from "@/components/input/input";
+import { NavigationSidebar } from "@/components/navigation/navigationSidebar";
+import ScoreValutForwarderJson from "@/contracts/mock/ScoreValutForwarder.sol/ScoreVaultForwarder.json";
+import ScoreValutJson from "@/contracts/mock/ScoreVault.sol/ScoreVault.json";
+import { ScoreVault, ScoreVaultForwarder } from "@/contracts/typechain-types";
+import { useEthersSigner } from "@/hooks/useEthersProvider";
+import { FORWARDER_CONTRACT_ADDRESS, SCOREVAULT_CONTRACT_ADDRESS } from "@/utils/contants";
+import { getUint48 } from "@/utils/getUint48";
+import { ForwardRequest } from "@/utils/types";
+import { Contract } from "ethers";
+import { useAccount, useSignTypedData } from "wagmi";
+
 
 export default function Evaluate() {
+
+  const account = useAccount();
+  const { signTypedDataAsync } = useSignTypedData();
+  // get Signer Instance
+  const signer: any = useEthersSigner();
+
+  /**
+   * setScore method
+   */
+  const setScore = async() => {
+    // create forwarder contract instance
+    const forwarder: ScoreVaultForwarder = (new Contract(FORWARDER_CONTRACT_ADDRESS, ScoreValutForwarderJson.abi, signer)) as any;
+    // create ScoreValut contract instance
+    const scoreVault: ScoreVault = (new Contract(SCOREVAULT_CONTRACT_ADDRESS, ScoreValutJson.abi, signer)) as any;
+    // get domain
+    const domain = await forwarder.eip712Domain();
+    // crate encodedFunctionData
+    const encodedData: any = scoreVault.interface.encodeFunctionData("setScore",[4, "testtest"])
+    // get unit48
+    const uint48Time = getUint48();
+    // create request data
+    const sig = await signTypedDataAsync({
+      domain: {
+        name: domain[1],
+        version: domain[2],
+        chainId: 534351, // scroll sepolia
+        verifyingContract: domain[4] as any
+      },
+      types: { 
+        'ForwardRequest': ForwardRequest 
+      },
+      primaryType:'ForwardRequest',
+      message: {
+        from: account.address,
+        to: SCOREVAULT_CONTRACT_ADDRESS,
+        value: 0,
+        gas: 360000,
+        nonce: await forwarder.nonces(account.address!),
+        deadline: uint48Time,
+        data: encodedData
+      },
+    });
+    // call requestRelayer API 
+    const gaslessResult = await fetch('/api/requestRelayer', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify({
+        from: account.address,
+        to: SCOREVAULT_CONTRACT_ADDRESS,
+        value: 0,
+        gas: 360000,
+        nonce: (await forwarder.nonces(account.address!)).toString(),
+        deadline: uint48Time.toString(),
+        data: encodedData,
+        signature: sig
+      }),
+    });
+
+    console.log(await gaslessResult.json())
+  }
+
   return (
     <div className="h-screen w-screen flex flex-row bg-white">
       <NavigationSidebar />
@@ -31,7 +104,10 @@ export default function Evaluate() {
           </div>
 
           <div>
-            <button className="group rounded-lg bg-Primary10 border-2 border-transparent hover:bg-Primary20 active:bg-Primary30 focus-visible:border-black disabled:bg-Primary40">
+            <button 
+              className="group rounded-lg bg-Primary10 border-2 border-transparent hover:bg-Primary20 active:bg-Primary30 focus-visible:border-black disabled:bg-Primary40"
+              onClick={async() => { await setScore() }}
+            >
               <div className="rounded-lg px-18 py-4 border border-transparent group-focus-visible:border-white">
                 <span className="text-Button text-white">Send</span>
               </div>

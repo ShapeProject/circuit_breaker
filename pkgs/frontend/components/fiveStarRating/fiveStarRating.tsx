e14ed895875d65c12092e3c4b24f193ba82b864f
K.N. <cg.yaruzo.material.daisuki@gmail.com>
2024-02-16 23:12:21 +0900

    Edit UI

--- pkgs/frontend/components/fiveStarRating/fiveStarRating.tsx
+++ pkgs/frontend/components/fiveStarRating/fiveStarRating.tsx
@@ -12,11 +12,11 @@ export const FiveStarRating: FC<PropsType> = ({ count, value, size, }: PropsType
     const avg = (value / count) * 100;
 
     return (
-        <>
-            <div className="relative w-fit">
-                <ul 
-                className="absolute top-0 left-0 z-10 flex flex-row items-center space-x-1 overflow-hidden" 
-                style={{ width: `${avg}%` }}>
+        <div className="w-fit rounded-2xl mx-auto px-8 py-4 border border-Primary10">
+            <div className="relative w-full [&>ul]:w-full [&>ul]:flex [&>ul]:flex-row [&>ul]:items-center">
+                <ul
+                    className="absolute top-0 left-0 z-10 overflow-hidden"
+                    style={{ width: `${avg}%` }}>
                     {...Array.from({ length: count }, (_, index) => (
                         <li>
                             <StarIcon key={index} width={size} height={size} className=" fill-Primary10" />
@@ -24,7 +24,7 @@ export const FiveStarRating: FC<PropsType> = ({ count, value, size, }: PropsType
                     ))}
                 </ul>
 
-                <ul className="flex flex-row items-center space-x-1">
+                <ul>
                     {...Array.from({ length: count }, (_, index) => (
                         <li>
                             <StarIcon key={index} width={size} height={size} className=" fill-Primary60" />
@@ -32,6 +32,6 @@ export const FiveStarRating: FC<PropsType> = ({ count, value, size, }: PropsType
                     ))}
                 </ul>
             </div>
-        </>
+        </div>
     );
 };
--- pkgs/frontend/pages/my-page.tsx
+++ pkgs/frontend/pages/my-page.tsx
@@ -1,5 +1,6 @@
 import { FiveStarRating } from "@/components/fiveStarRating/fiveStarRating";
 import { NavigationSidebar } from "@/components/navigation/navigationSidebar";
+import { ScoreCircle } from "@/components/scoreCircle";
 import ScoreValutJson from "@/contracts/mock/ScoreVault.sol/ScoreVault.json";
 import { SCOREVAULT_CONTRACT_ADDRESS } from "@/utils/contants";
 import { readContract } from "@wagmi/core";
@@ -7,7 +8,7 @@ import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
 export default function MyPage() {
-  
+
   const [encrptedScore, setEncyrptedScore] = useState("");
 
   const account = useAccount();
@@ -16,8 +17,8 @@ export default function MyPage() {
     /**
      * init method
      */
-    const init = async() => {
-      if(account.address != undefined) {
+    const init = async () => {
+      if (account.address != undefined) {
         // get encryptedScore
         const result = await readContract({
           address: SCOREVAULT_CONTRACT_ADDRESS,
@@ -34,40 +35,35 @@ export default function MyPage() {
   return (
     <div className="h-screen w-screen flex flex-row">
       <NavigationSidebar />
-      <div className="h-full w-full flex flex-row px-10">
-        <div className="w-fit rounded-t-2xl flex flex-col space-y-14 px-10 py-14 bg-white shadow-lg">
-          <h1 className="text-Title mx-auto">My Page</h1>
-          <div className="px-10 py-6 space-y-10">
-            <div className="space-y-6 flex flex-col">
-              <span className="text-BodyStrong text-Primary40">Total Score</span>
-              <span className="w-64 text-BodyMono text-right">1,105</span>
-            </div>
-            <div className="space-y-6 flex flex-col">
-              <span className="text-BodyStrong text-Primary40">Received</span>
-              <span className="w-64 text-BodyMono text-right">13</span>
-            </div>
-          </div>
-          <FiveStarRating
-          value={3.2}
-          count = {5}
-          size={40}
-          />
-        </div>
-        <div className="relative p-10 [&_div]:flex [&_div]:justify-center [&_div]:items-center">
-          <div className="h-full aspect-square rounded-full bg-Gray30">
-            <div className="h-68pct aspect-square rounded-full bg-white shadow-md">
-              <div className="h-89pct aspect-square rounded-full border-40 border-Gray20 border-dashed">
-                <div className="h-92pct aspect-square rounded-full border-24 border-Gray20">
-                  <div className="h-92pct aspect-square rounded-full border-12 border-Gray20">
-                    <div className="h-92pct aspect-square rounded-full bg-Primary10 shadow-lg">
-                      <span className="font-mono text-AvgScore text-white">85</span>
-                    </div>
-                  </div>
-                </div>
+      <div className="h-full w-full flex flex-row px-10 justify-between">
+        <div className="h-full flex items-end">
+          <div className="w-fit h-fit rounded-t-2xl flex flex-col space-y-14 px-10 py-14 bg-white shadow-lg">
+            <h1 className="text-Title mx-auto">My Page</h1>
+            <div className="px-10 py-6 space-y-10">
+              <div className="space-y-6 flex flex-col">
+                <span className="text-BodyStrong text-Primary40">Total Score</span>
+                <span className="w-full text-BodyMono text-right">1,105</span>
+              </div>
+              <div className="space-y-6 flex flex-col">
+                <span className="text-BodyStrong text-Primary40">Received</span>
+                <span className="w-full text-BodyMono text-right">13</span>
               </div>
             </div>
+            <FiveStarRating
+              value={4.1}
+              count={5}
+              size={40}
+            />
           </div>
         </div>
+
+        <div className="relative p-10 [&_div]:flex [&_div]:justify-center [&_div]:items-center">
+
+          <ScoreCircle />
+          
+        </div>
+
+
       </div>
     </div>
   );

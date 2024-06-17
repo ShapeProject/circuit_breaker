1fa27b4ba5a1a7b0de9cc1fd89f3cf97a2c90200
Mameta29 <fc.kikkawa@gmail.com>
2024-06-17 16:36:41 +0900

    feature/front fix loading

--- pkgs/frontend/pages/_app.tsx
+++ pkgs/frontend/pages/_app.tsx
@@ -27,7 +27,7 @@ const { connectors } = getDefaultWallets({
 });
 
 const wagmiConfig = createConfig({
-  // autoConnect: true,
+  autoConnect: true,
   connectors,
   publicClient,
   webSocketPublicClient,
